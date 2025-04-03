037102ead8bafb3ef4f1969fe441d6a926bfcdba
Meme Chhay <meme.chhay@ucdenver.edu>
2025-04-02 20:02:34 -0600

    Syntax error fixed

--- Movies.js
+++ Movies.js
@@ -8,33 +8,36 @@ const connectDB = async () => {
     console.log("Connected to MongoDB-movies");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit the process if the connection fails (optional)
+    process.exit(1);
   }
 };
 
 connectDB();
 
 // Movie schema
-
 const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true, index: true },
-  releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+  releaseDate: {
+    type: Number,
+    min: [1900, 'Must be greater than 1899'],
+    max: [2100, 'Must be less than 2100']
+  },
   genre: {
     type: String,
     enum: [
-      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy',
+      'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
     ],
   },
   actors: [{
     actorName: String,
     characterName: String,
   }],
-  imageURL: { 
-    type: String, 
-    default: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"
-    index: true },
+  imageURL: {
+    type: String,
+    default: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
+    index: true
+  },
 });
-
-
 
 module.exports = mongoose.model('Movie', MovieSchema);
