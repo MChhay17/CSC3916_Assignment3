1ea98d17cee56be082555682540ab2952b81100a
Meme Chhay <meme.chhay@ucdenver.edu>
2025-04-20 16:44:03 -0600

    movies updated

--- Movies.js
+++ Movies.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -8,36 +7,33 @@ const connectDB = async () => {
     console.log("Connected to MongoDB-movies");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1);
+    process.exit(1); // Exit the process if the connection fails (optional)
   }
 };
 
 connectDB();
 
 // Movie schema
+
 const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true, index: true },
-  releaseDate: {
-    type: Number,
-    min: [1900, 'Must be greater than 1899'],
-    max: [2100, 'Must be less than 2100']
-  },
+  releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
   genre: {
     type: String,
     enum: [
-      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy',
-      'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
     ],
   },
   actors: [{
     actorName: String,
     characterName: String,
   }],
-  imageURL: {
-    type: String,
-    default: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
-    index: true
-  },
+  imageURL: { 
+    type: String, 
+    default: "https://wallpapercave.com/wp/wp5338281.jpg", // ✅ Correct default value syntax
+    index: true },
 });
+
+
 
 module.exports = mongoose.model('Movie', MovieSchema);
