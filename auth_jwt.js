669ecc534d50225d00bb94287d138b4e60dfac4d
Meme Chhay <meme.chhay@ucdenver.edu>
2025-04-20 20:57:29 -0600

    updated auth

--- auth_jwt.js
+++ auth_jwt.js
@@ -1,25 +1,26 @@
-var passport = require('passport');
-var JwtStrategy = require('passport-jwt').Strategy;
-var ExtractJwt = require('passport-jwt').ExtractJwt;
-var User = require('./Users');
-require('dotenv').config(); 
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('./Users');
+require('dotenv').config();
 
-var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // ✅ correct
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRET_KEY;
 
-passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
     try {
-        const user = await User.findById(jwt_payload.id);
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-        }
-    } catch (err) {
-        return done(err, false);
-    }
-}));
+      // FIX: Find user by username so we get req.user.username
+      const user = await User.findOne({ username: jwt_payload.username });
 
-exports.isAuthenticated = passport.authenticate('jwt', { session : false });
+      if (user) return done(null, user);
+      else return done(null, false);
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+exports.isAuthenticated = passport.authenticate('jwt', { session: false });
 exports.secret = opts.secretOrKey;
