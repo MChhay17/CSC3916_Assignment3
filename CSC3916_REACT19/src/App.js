4c9c20d8038abe35d461526d53cbc8f34858bcab
Meme Chhay <meme.chhay@ucdenver.edu>
2025-04-20 18:31:50 -0600

    Set up routing

--- CSC3916_REACT19/src/App.js
+++ CSC3916_REACT19/src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
-import MovieHeader from './components/movieheader';
-import MovieList from './components/movielist';
-import Movie from './components/movie';
-import Authentication from './components/authentication';
-import {HashRouter, Routes,  Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import MovieListPage from './pages/MovieListPage';
+import MovieDetailPage from './pages/MovieDetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <HashRouter> {/* The Router component */}
-        <MovieHeader />
-        <Routes>
-          <Route path="/" element={<MovieList />} />
-          <Route path="/movielist" element={<MovieList />}/>
-          <Route path="/movie/:movieId" element={<Movie />}/>
-          <Route path="/signin" element={<Authentication />}/>
-          {/*... other routes */}
-        </Routes>
-      </HashRouter>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MovieListPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/movies/:id" element={<MovieDetailPage />} />
+      </Routes>
+    </Router>
   );
 }
 
